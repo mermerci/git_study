aade5fb894cc22c3eb27768a468b130a88a9720c
Ilak0121 <s7515199@gmail.com>
2019-02-02 15:58:33 +0900

    testing for conflict

--- practice1/cal.cpp
+++ practice1/cal.cpp
@@ -6,18 +6,5 @@ auto minus(auto a, auto b){
 }
 
 int main(){
-    int x, y, z;
-
-    cout << "숫자 뺄셈" << endl;
- 
-    cout << "x값 입력:";
-    cin >> x;
-
-    cout << "y값 입력:";
-    cin >> y;
-
-    z = x-y;
-    cout << "결과: " << z << endl;
-
     return 0;
 }
